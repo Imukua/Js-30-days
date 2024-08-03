b2f4becd53be133cbfa3acd6247427f4322ea94b
Imukua <ianmukuaa@gmail.com>
2024-08-03 21:22:26 +0300

    initialised app

--- Javascript-Drumset/tailwind.config.js
+++ Javascript-Drumset/tailwind.config.js
@@ -1,8 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './public/index.html',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'body':['Nunito']
+      },
+      colors: {
+        'background': '#F3F9D2',
+        't-green':'#CBEAA6',
+        'pistachio':'#C0D684',
+        'ppl':'#3D0B37',
+        'violet':'#63264A',
+      }
+    },
   },
   plugins: [],
 }
